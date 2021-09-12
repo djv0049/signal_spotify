25b58a7803f442aa5327ca693bb98dcef08ca7b6
DanielvanHoof <danielv@cleverfirstaid.com>
2021-09-12 13:24:52 +1200

    On branch master  Initial commit  Changes to be committed: 	new file:   __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+    print("hello world")
+    print("here, I will create a connection to a class that will establish connection to the signal service")
+
